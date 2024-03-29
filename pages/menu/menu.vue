af3da011c390f1ebdd727b3f41f83ea086482ae0
lvwei1 <lvwei1@eastmoney.com>
2024-03-29 16:45:28 +0800

    分类和下单

--- pages/menu/menu.vue
+++ pages/menu/menu.vue
@@ -1,20 +1,25 @@
 <template>
 	<view class="container" style="background-color: pink;">
 		<uni-row :gutter="0">
-			<uni-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1" style=" background-color: aqua;">
-				<scroll-view :scroll-top="scrollTop" scroll-y="true" class="scroll-Y">
-					<block v-for="(item, index) in categorys" :key="index">
+			<uni-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1" style=" background-color: aqua;"
+				class="fixed-scroll-category">
+				<scroll-view :scroll-top="scrollTopCategory" scroll-y="true" class="scroll-Y scroll-category"
+					:show-scrollbar="false">
+					<uni-card class="category-card" :class="{'selected': item.categoryId === selectCardId}"
+						v-for="(item, index) in categorys" @click="itemIze(item)" :key="item.categoryId">
 						<view>
 							<text>{{item.categoryName}}</text>
 						</view>
-					</block>
+					</uni-card>
 				</scroll-view>
 			</uni-col>
-			<uni-col :xs="16" :sm="18" :md="20" :lg="21" :xl="23">
-				<scroll-view :scroll-top="scrollTop" scroll-y="true" class="scroll-Y" @scrolltoupper="upper"
-					@scrolltolower="lower" @scroll="scroll">
-					<block v-for="(item, index) in menus" :key="index" class="block-item">
-						<view class="good-detail">
+
+			<uni-col :xs="16" :sm="18" :md="20" :lg="21" :xl="23" class="scroll-menu-container">
+				<scroll-view :scroll-top="scrollTop" scroll-y="true" class="scroll-Y scroll-menu" @scrolltoupper="upper"
+					@scrolltolower="lower" @scroll="scroll" :show-scrollbar="true" ref="scrollMenu"
+					:scroll-into-view="scroll_into" style="height: 1500rpx;">
+					<view v-for="(item, index) in menus" :key="index" class="block-item" :id="'menuId-' +item.id">
+						<view class="good-detail" :id="item.categoryId">
 							<swiper class="good-img-swiper" circular :indicator-dots="true" :autoplay="true"
 								:interval="2000" :duration="500">
 								<swiper-item v-for="(img, sIndex) in item.pics" :key="sIndex">
@@ -34,7 +39,7 @@
 								<uni-number-box :min="0" v-model="item.quantity" @change="changeValue(item)" />
 							</view>
 						</view>
-					</block>
+					</view>
 				</scroll-view>
 			</uni-col>
 		</uni-row>
@@ -72,9 +77,14 @@
 					</view>
 				</block>
 			</scroll-view>
-			<u-button type="error" :disabled="this.cart.length == 0" color="linear-gradient(to right, rgb(255, 170, 255), rgb(213, 51, 186))">下单</u-button>
+			<u-button type="error" :disabled="this.cart.length == 0"
+				color="linear-gradient(to right, rgb(255, 170, 255), rgb(213, 51, 186))"
+				@click="createOrder">下单</u-button>
 		</u-popup>
+
+		<u-toast ref="uToast" />
 	</view>
+
 
 
 </template>
@@ -84,6 +94,10 @@
 	export default {
 		data() {
 			return {
+				scroll_into: '',
+				scrollTop: 0,
+				scrollTopCategory: 0,
+				selectCardId: '',
 				show: false,
 				pattern: {
 					backgroundColor: "pink",
@@ -91,7 +105,6 @@
 					icon: "cart",
 				},
 				cartShow: true,
-				scrollTop: 0,
 				name: '',
 				shopId: 15,
 				getReqParam: {
@@ -110,8 +123,16 @@
 			this.getData();
 		},
 		methods: {
+			reset() {
+				this.clearData();
+				this.getData();
+			},
+			// 点击类目加上背景色
+			itemIze(item) {
+				this.selectCardId = item.categoryId;
+				this.menus = item.menus;
+			},
 			changeValue(item) {
-				console.log(item);
 				let index = this.cart.findIndex(i => i.id === item.id);
 				if (index === -1) {
 					this.cart.push(item);
@@ -140,17 +161,15 @@
 			clearData() {
 				this.categorys = [];
 				this.menus = [];
+				this.close();
 			},
 			getData() {
 				request('/menu/list?shopId=' + this.shopId, 'GET', {}, 'menu')
 					.then((res) => {
 						console.log('data', res);
 						this.categorys = res.data;
-						for (let i = res.data.length - 1; i >= 0; i--) {
-							this.menus.push(
-								...res.data[i].menus
-							);
-						}
+						this.menus = this.categorys[0].menus;
+						this.selectCardId = this.categorys[0].categoryId;
 					}).catch((err) => {
 						console.log(err)
 						uni.showToast({
@@ -159,46 +178,72 @@
 							duration: 1500
 						})
 					});
-				// uni.request({
-				// 	url: 'http://localhost:8081/menu/list?shopId=' + this.shopId,
-				// 	method: 'GET',
-				// 	header: {
-				// 		'token': this.token
-				// 	},
-				// 	success: (res) => {
-				// 		console.log(res);
-				// 		if (res.data.code != 0) {
-				// 			uni.showToast({
-				// 				title: res.data.message,
-				// 				icon: 'error',
-				// 				duration: 1500
-				// 			})
-				// 			return;
-				// 		}
-				// 		this.categorys = res.data.data;
-				// 	},
-				// 	fail: (err) => {
-				// 		console.log(err);
-				// 		uni.showToast({
-				// 			title: err.errMsg,
-				// 			icon: 'error',
-				// 			duration: 1500
-				// 		})
-				// 	}
-				// })
 			},
-			upper(e) {},
-			scroll(e) {
-				console.log(e)
-			},
-			lower() {
 
-			}
+			createOrder() {
+				let ids = [];
+				this.cart.forEach(f => {
+					let m = {
+						id: f.id,
+						quantity: f.quantity
+					};
+					ids.push(m);
+				})
+				let data = {
+					'shopId': 15,
+					'menuIds': ids
+				};
+				console.log('req', data);
+				request('/order', 'POST', data, 'menu')
+					.then((res) => {
+						console.log('order', res);
+						if (res.code === 0) {
+							uni.showToast({
+								title: '下单成功',
+								icon: 'success',
+								duration: '1000'
+							});
+							this.show = false;
+							uni.switchTab({
+								url: '/pages/order/order?id=' + res.data
+							});
+						}
+					}).catch((err) => {
+						console.log('err', err)
+						this.show = false;
+						uni.showToast({
+							title: err.data.message,
+							icon: 'error',
+							duration: 1500
+						});
+					});
+			},
+
+			upper(e) {},
 		}
 	}
 </script>
 
 <style>
+	.selected {
+		border-color: #007aff;
+		/* 高亮边框颜色 */
+		box-shadow: 0 0 10px rgba(0, 123, 255, 0.2);
+		/* 阴影效果 */
+		background-color: #f0f9ff;
+		/* 背景颜色 */
+	}
+
+	.fixed-scroll-category {
+		position: fixed;
+	}
+
+	.scroll-menu {}
+
+	.scroll-menu-container {
+		margin-left: 33%;
+	}
+
 	.good-detail2 {
 		display: flex;
 		border-bottom: 1rpx solid rosybrown;
